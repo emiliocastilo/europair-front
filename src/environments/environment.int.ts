bcf93be05a641e5c9f9a222f1c285e90be9ddc44
David Castro Aguilar <david.castroaguilar@plexus.es>
2020-07-22 10:18:36 +0100

    fix env path

--- src/environments/environment.int.ts
+++ src/environments/environment.int.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "http://192.168.210.22/services"
+  apiUrl: "http://192.168.210.22/services/"
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://productionserver/services"
+  apiUrl: "http://productionserver/services/"
 };
