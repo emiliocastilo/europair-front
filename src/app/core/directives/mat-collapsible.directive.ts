8b5bbea3483735b02b95309bb822b976f6b9bfed
Elias Barreiro <elias.barreirosuris@plexus.es>
2020-07-27 11:27:51 +0200

    Added Renderer2 to DOM manipulation - Refactor of methods and magic strings

--- src/app/core/directives/mat-collapsible.directive.ts
+++ src/app/core/directives/mat-collapsible.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, OnDestroy, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appMatCollapsible]',
@@ -6,8 +6,11 @@ import { Directive, ElementRef, OnDestroy } from '@angular/core';
 export class MatCollapsibleDirective implements OnDestroy {
   private matCollapsibleInstance: M.Collapsible;
 
-  constructor(elementRef: ElementRef) {
-    this.matCollapsibleInstance = M.Collapsible.init(elementRef.nativeElement, {
+  private readonly openIcon = 'keyboard_arrow_down';
+  private readonly closeIcon = 'keyboard_arrow_up';
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {
+    this.matCollapsibleInstance = M.Collapsible.init(this.elementRef.nativeElement, {
       onCloseEnd: this.onCloseCallback,
       onOpenEnd: this.onOpenCallback,
     });
@@ -15,13 +18,17 @@ export class MatCollapsibleDirective implements OnDestroy {
 
   private onCloseCallback = (el: Element) => {
     const collapsibleIcon = el.getElementsByClassName('material-icons')[0];
-    collapsibleIcon.innerHTML = 'keyboard_arrow_down';
+    this.changeIcon(collapsibleIcon, this.openIcon);
   };
 
   private onOpenCallback = (el: Element) => {
     const collapsibleIcon = el.getElementsByClassName('material-icons')[0];
-    collapsibleIcon.innerHTML = 'keyboard_arrow_up';
+    this.changeIcon(collapsibleIcon, this.closeIcon);
   };
+
+  private changeIcon(collapsibleIcon: Element, icon: string) {
+    this.renderer.setProperty(collapsibleIcon, 'innerHTML', icon);
+  }
 
   ngOnDestroy(): void {
     this.matCollapsibleInstance.destroy();
