ed269c5248e66e82ffad2efd2aa67d5f1f330eee
Emilio <emilio.castillogonzalez@plexus.es>
2020-09-09 11:53:31 +0200

    Añadimos campo 'Pertenece a la UE' a paises

--- src/app/modules/dashboards/masters/countries/models/country.ts
+++ src/app/modules/dashboards/masters/countries/models/country.ts
@@ -4,12 +4,14 @@ export interface Country extends Audit {
     id: number;
     code?: string;
     name?: string;
+    europeanUnion?: boolean;
 }
 
 export const EMPTY_COUNTRY: Country = {
     id: undefined,
     code: '',
     name: '',
+    europeanUnion: false,
     createdAt: null,
     createdBy : null,
     modifiedAt : null,
