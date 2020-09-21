9e2eaa3d1aabda08a899275d09c6286bdd3a618a
Pablo Alves <pablo.alvessampaio@plexus.es>
2020-09-21 17:10:31 +0200

    feat: file's screen corrections

--- src/app/modules/dashboards/files/models/FileRoute.model.ts
+++ src/app/modules/dashboards/files/models/FileRoute.model.ts
@@ -2,9 +2,26 @@ export interface FileRoute {
   id: number;
   code: string;
   frequency: FrequencyType;
+  frequencyDays: FrequencyDay[];
   initialDate: Date;
   endDate: Date;
   rotations: FileRoute[];
+}
+
+export interface FrequencyDay {
+  id: number;
+  weekday: DayOfWeek;
+  monthDay: number;
+}
+
+export enum DayOfWeek {
+  MONDAY = 1,
+  TUESDAY = 2,
+  WEDNESDAY = 3,
+  THURSDAY = 4,
+  FRIDAY = 5,
+  SATURDAY = 6,
+  SUNDAY = 7,
 }
 
 export enum FrequencyType {
@@ -22,3 +39,13 @@ export const FREQUENCY_LIST = [
   FrequencyType.DAY_OF_MONTH,
   FrequencyType.WEEKLY,
 ];
+
+export const DAYS_LIST = new Map<DayOfWeek, string>([
+  [DayOfWeek.MONDAY, 'MONDAY'],
+  [DayOfWeek.TUESDAY, 'TUESDAY'],
+  [DayOfWeek.WEDNESDAY, 'WEDNESDAY'],
+  [DayOfWeek.THURSDAY, 'THURSDAY'],
+  [DayOfWeek.FRIDAY, 'FRIDAY'],
+  [DayOfWeek.SATURDAY, 'SATURDAY'],
+  [DayOfWeek.SUNDAY, 'SUNDAY'],
+]);
