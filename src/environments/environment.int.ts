d676cb9e1f50ee79f448345973883e4ca0ba4e2a
Ivan Jesus Fanjul Cosent <ivan.fanjulcosent@plexus.es>
2020-09-24 18:10:33 +0200

    Update environment.int.ts

--- src/environments/environment.int.ts
+++ src/environments/environment.int.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: "https://dev-erp-europair.plexus.services/services/",
+  apiUrl: "http://dev-erp-europair.plexus.services/services/",
   mock: false
 };
