27140685d2720db604e1bb8e102e84044c85af31
Elias Barreiro <elias.barreirosuris@plexus.es>
2020-10-08 14:43:22 +0200

    Feat add enviroment oAuthConfig
    Create enviroment oAuthConfig object

--- src/app/core/models/auth.config.ts
+++ src/app/core/models/auth.config.ts
@@ -1,12 +1,4 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
+import { environment } from 'src/environments/environment';
 
-export const oAuthConfig: AuthConfig = {
-  issuer:
-    'https://login.microsoftonline.com/aa0ccb90-70ce-4fe7-bac0-c68c12be8ab1/v2.0',
-  redirectUri: window.location.origin + '/login',
-  postLogoutRedirectUri: window.location.origin + '/login',
-  clientId: 'bfd88018-a688-4d7a-a72e-d08e159bee39',
-  responseType: 'code',
-  strictDiscoveryDocumentValidation: false,
-  scope: 'openid api://bfd88018-a688-4d7a-a72e-d08e159bee39/app profile',
-};
+export const oAuthConfig: AuthConfig = environment.oAuthConfig;
