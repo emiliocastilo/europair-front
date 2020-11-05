ec419d6abac8706292b05b39a69a85b6dfcdff9b
Elias Barreiro <elias.barreirosuris@plexus.es>
2020-11-05 11:36:40 +0100

    Feat add remove button to services table
    Move mat svg icons initialize from files to dashboards.

--- src/app/modules/dashboards/masters/services/services.component.ts
+++ src/app/modules/dashboards/masters/services/services.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { ConfirmOperationDialogComponent } from 'src/app/core/components/dialogs/confirm-operation-dialog/confirm-operation-dialog.component';
 import { Page } from 'src/app/core/models/table/pagination/page';
 import { Services } from './models/services.model';
 import { ServicesService } from './services/services.service';
@@ -17,14 +19,15 @@ export class ServicesComponent implements OnInit {
   public services: Services[];
   public serviceDetailTitle: string;
 
-  public columnToDisplay = ['code', 'name', 'type'];
+  public columnToDisplay = ['code', 'name', 'type', 'actions'];
   public dataSource = new MatTableDataSource();
   public resultsLength: number = 0;
   public pageSize: number = 0;
   
   constructor(
     private readonly servicesService: ServicesService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly matDialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -42,6 +45,26 @@ export class ServicesComponent implements OnInit {
 
   public createService(): void {
     this.router.navigate(['services', 'new']);
+  }
+
+  public deleteService(service: Services) {
+    const confirmOperationRef = this.matDialog.open(
+      ConfirmOperationDialogComponent,
+      {
+        data: {
+          title: 'SERVICES.CONFIRM_DELETE',
+          message: 'SERVICES.MSG_DELETE_SERVICE',
+          translationParams: {
+            name: service.name,
+          },
+        },
+      }
+    );
+    confirmOperationRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.servicesService.deleteServices(service).subscribe(() => this.initializeServicesTable());
+      }
+    });
   }
 
   public goToDetail(service: Services): void {
