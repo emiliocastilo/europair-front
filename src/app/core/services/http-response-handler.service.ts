0bff60e6fa14ae8a422217b1c77c52d957de0bb9
axel.sanchez <axel.sanchezsalom@plexus.es>
2021-01-27 18:30:43 +0100

    [WIP] Airport fixes

--- src/app/core/services/http-response-handler.service.ts
+++ src/app/core/services/http-response-handler.service.ts
@@ -2,19 +2,33 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslatedSnackBarComponent } from '../components/snack-bar/translated-snack-bar/translated-snack-bar.component';
+import { ErrorCodes, getErrorTranslationLabel } from '../models/error/error-codes';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpResponseHandlerService {
-  constructor(private _snackBar: MatSnackBar) {}
+  constructor(private _snackBar: MatSnackBar) { }
 
   public manageError(error: HttpErrorResponse) {
+
+    let errorMsg: string = error.error.message;
+    // ToDo: acabar traduccion de todos los errores y poner mensaje genérico para errores no controlados.
+    let errorLabel = errorMsg;
+
+    if (errorMsg.startsWith('[')) {
+      let errorCode: string = errorMsg.slice(1, 4);
+      let translationLabel = getErrorTranslationLabel(errorCode);
+      if (translationLabel) {
+        errorLabel = 'ERRORS.CODES.' + translationLabel;
+      }
+    }
+
     this._snackBar.openFromComponent(TranslatedSnackBarComponent, {
       duration: 5000,
       panelClass: ['ep-snackbar-error'],
       data: {
-        message: error.error.message,
+        message: errorLabel
       },
     });
   }
